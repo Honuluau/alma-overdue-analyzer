ee3e78267b1fe88c1362d9597bc812073f52713c
honuluau <honu.dylan@gmail.com>
2025-08-14 08:23:03 -0400

    Inserting values into tables and committing them.

--- testing/SQLite3 demo.py
+++ testing/SQLite3 demo.py
@@ -1,3 +1,15 @@
 import sqlite3
+# Following along with the https://docs.python.org/3/library/sqlite3.html tutorial.
+
 con = sqlite3.connect("example/tutorial.db")
-# Directory of the database file.
+cur = con.cursor()
+
+# cur.execute("CREATE TABLE movie(title, year, score)")
+
+cur.execute("""
+    INSERT INTO movie VALUES
+        ('Monty Python and the Holy Grail', 1975, 8.2),
+        ('And Now for Something Completely Different', 1971, 7.5)
+""")
+
+con.commit()
