34537b8f4927e621520382d4314d8f7d97b323d2
honuluau <honu.dylan@gmail.com>
2025-08-14 09:19:25 -0400

    Revised database that includes a new patron table that simplifies the transaction table.

--- testing/Prototype.py
+++ testing/Prototype.py
@@ -16,18 +16,26 @@ def create_tables():
     """)
 
     cur.execute("""
+        CREATE TABLE patrons (
+            patron_id INTEGER PRIMARY KEY,
+            eagle_id VARCHAR(32),
+            first_name VARCHAR(50),
+            last_name VARCHAR(50),
+            preferred_email VARCHAR(100)
+        )
+    """)
+
+    cur.execute("""
         CREATE TABLE transactions (
             transaction_id INTEGER PRIMARY KEY,
             item_id INTEGER NOT NULL,
             process_status VARCHAR(8) NOT NULL DEFAULT 'LOST' CHECK (process_status IN ('LOST','RETURNED')),
             process_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            first_name VARCHAR(50),
-            last_name VARCHAR(50),
-            eagle_id VARCHAR(32),
-            preferred_email VARCHAR(100),
+            patron_id INTEGER NOT NULL,
             loan_date DATE,
             due_date DATE,
-            FOREIGN KEY (item_id) REFERENCES items(item_id)   
+            FOREIGN KEY (item_id) REFERENCES items(item_id),
+            FOREIGN KEY (patron_id) REFERENCES patrons(patron_id)
         )
     """)
 
