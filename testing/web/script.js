47a8e44c654090ce09c43cb146268c338a8533b1
honuluau <honu.dylan@gmail.com>
2025-08-15 11:43:59 -0400

    I'm gonna focus on backend now because front-end is so foreign to me.

--- testing/web/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('update_button');
-    btn.addEventListener('click', () => {
-        // Call Python function exposed via js_api
-        pywebview.api.change_text();
-    });
-});
