5a6c67b0e05a6e7633a6312856d5315e278aa482
honuluau <honu.dylan@gmail.com>
2025-08-15 09:18:40 -0400

    GUI Prototype - Log System

--- testing/GUI Prototype.py
+++ testing/GUI Prototype.py
@@ -1,9 +1,25 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
+import datetime
+
+def get_file_path():
+    return askopenfilename(title="Select Fulfillment - Loans Returns and Overdue Dashboard", filetypes=[("Fulfillment Report", "*.xlsx")])
+
+def log(log_type, message):
+    try:
+        with open("log/log.txt", 'a+') as f:
+            current_datetime = datetime.datetime.now()
+            f.writelines(f"\n[{current_datetime.strftime("%Y-%m-%d %H:%M:%S")}][{log_type}] {message}")
+    except FileExistsError:
+        print(f"File already exists.")
+    except Exception as e:
+        print(f"An error has occurred: {e}")
+
+def main():
+    log("TEST", "This is a test")
+
+main()
 
 window = Tk()
-
-filename = askopenfilename(title="Select Fulfillment - Loans Returns and Overdue Dashboard", filetypes=[("Fulfillment Report", "*.xlsx")])
-print(filename)
 
 window.mainloop()
