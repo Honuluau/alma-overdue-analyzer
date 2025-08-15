e412bf818f1102c8440fbe7c6d1cba84a23908b3
honuluau <honu.dylan@gmail.com>
2025-08-15 09:34:46 -0400

    GUI Prototype - Get last run time

--- testing/GUI Prototype.py
+++ testing/GUI Prototype.py
@@ -10,13 +10,32 @@ def log(log_type, message):
         with open("log/log.txt", 'a+') as f:
             current_datetime = datetime.datetime.now()
             f.writelines(f"\n[{current_datetime.strftime("%Y-%m-%d %H:%M:%S")}][{log_type}] {message}")
-    except FileExistsError:
-        print(f"File already exists.")
     except Exception as e:
         print(f"An error has occurred: {e}")
 
+def get_last_run_time():
+    try:
+        with open("log/log.txt") as f:
+            lines = f.readlines()
+            last_run_time = None
+            for line in lines[::-1]:
+                if "RAN" in line:
+                    last_run_time = line.split("]")[0].strip("[]")
+                    break
+
+            f.close()
+
+            if last_run_time is None:
+                log("ERROR", "Error code 2. None returned.")
+
+            return last_run_time
+
+    except Exception as e:
+        log("ERROR", f"Error code 1. {e}")
+
 def main():
-    log("TEST", "This is a test")
+    last_run_time = get_last_run_time()
+    print(last_run_time)
 
 main()
 
